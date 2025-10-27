cb6364e8aa6fe6e09f7b1d6fe00ca158119d7de8
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2025-10-27 18:34:54 +0530

    refactor(shared): centralize messages and  ErrorMessages for consistent error handling

---
+++ src/shared/constants/ErrorMessages.ts
@@ -0,0 +1,25 @@
+
+
+
+export const ErrorMessages = {
+
+    USER: {
+        USER_NOT_FOUND: "User not found",
+        INVALID_CREDENTIALS: "Invalid credentials",
+        USER_ALREADY_EXISTS: "User already exists",
+    },
+    AUTH: {
+        INVALID_TOKEN: "Invalid token",
+        TOKEN_EXPIRED: "Token has expired",
+        OTP_EXPIRED: "OTP has expired",
+        INVALID_OTP: "Invalid OTP",
+    },
+    SYSTEM:{
+        INTERNAL_SERVER_ERROR: "Internal server error",
+        BAD_REQUEST: "Bad request",
+        NOT_FOUND: "Not found",
+        UNAUTHORIZED: "Unauthorized",
+    }
+
+    
+};
---
+++ src/shared/constants/Message.ts
@@ -0,0 +1,18 @@
+
+
+const Messages={
+
+    USER:{
+        CREATED:"User created successfully",
+        LOGIN:"User logged in successfully",
+        GOOGLE_AUTH:"User authenticated via Google successfully",
+        OTP_SENT:"OTP sent successfully",
+        PASSWORD_RESET:"Password reset successfully",
+    },
+    AUTH:{
+        OTP_VERIFIED:"OTP verified successfully",
+        TOKEN_REFRESHED:"Token refreshed successfully",
+    }
+
+
+}
