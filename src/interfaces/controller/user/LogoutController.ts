221362159c184eebad19b2b5c5db547fd32eb64c
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2025-10-27 19:50:18 +0530

    refractor:missed try catch added in logoutcontroller

--- src/interfaces/controller/user/LogoutController.ts
+++ src/interfaces/controller/user/LogoutController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { HttpStatusCode } from '@/shared/constants/HttpStatusCode';
 import { env } from '@/config/env';
+import { logger } from '@/infrastructure/logger/WinstonLogger/logger';
 
 
 
@@ -8,25 +9,30 @@ import { env } from '@/config/env';
 
 
 export class LogoutController {
-  
+
   logout = (req: Request, res: Response): Response => {
-  
-    const isProduction = env.NODE_ENV === "production"
+    try {
+      const isProduction = env.NODE_ENV === "production"
+      
+      res.clearCookie('accessToken', {
+        httpOnly: true,
+        sameSite: isProduction ? "none" : "strict",
+        secure: isProduction,
+        domain: isProduction ? env.COOKIE_DOMAIN : undefined,
 
-    res.clearCookie('accessToken', {
-      httpOnly: true,
-      sameSite: isProduction ? "none" : "strict",
-      secure: isProduction,
-      domain: isProduction ? env.COOKIE_DOMAIN : undefined,
+      });
 
-    });
+      res.clearCookie('refreshToken', {
+        httpOnly: true,
+        sameSite: isProduction ? "none" : "strict",
+        secure: isProduction,
+        domain: isProduction ? env.COOKIE_DOMAIN : undefined,
+      });
+      return res.status(HttpStatusCode.OK).json({ message: 'Logged out successfully' });
+    } catch (error) {
+      logger.error('Error during logout:', { error });
+      return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+    }
+  }
 
-    res.clearCookie('refreshToken', {
-      httpOnly: true,
-      sameSite: isProduction ? "none" : "strict",
-      secure: isProduction,
-      domain: isProduction ? env.COOKIE_DOMAIN : undefined,
-    });
-    return res.status(HttpStatusCode.OK).json({ message: 'Logged out successfully' });
-  };
 }
