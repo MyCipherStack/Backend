0fbf7322701ae645e5e71288fb61e775c6ca3ef6
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2025-10-28 10:39:24 +0530

    fix(logout):path added in logout

--- src/interfaces/controller/user/LogoutController.ts
+++ src/interfaces/controller/user/LogoutController.ts
@@ -13,21 +13,19 @@ export class LogoutController {
   logout = (req: Request, res: Response): Response => {
     try {
       const isProduction = env.NODE_ENV === "production"
+
+
+      const cookieOptions = {
+        httpOnly: true,
+        sameSite: isProduction ? "none" : "strict" as "none" |  "strict",
+        secure: isProduction,
+        domain: isProduction ? env.COOKIE_DOMAIN : undefined,
+        path: "/"
+      }
+
+      res.clearCookie('accessToken', cookieOptions);
+      res.clearCookie('refreshToken', cookieOptions);
       
-      res.clearCookie('accessToken', {
-        httpOnly: true,
-        sameSite: isProduction ? "none" : "strict",
-        secure: isProduction,
-        domain: isProduction ? env.COOKIE_DOMAIN : undefined,
-
-      });
-
-      res.clearCookie('refreshToken', {
-        httpOnly: true,
-        sameSite: isProduction ? "none" : "strict",
-        secure: isProduction,
-        domain: isProduction ? env.COOKIE_DOMAIN : undefined,
-      });
       return res.status(HttpStatusCode.OK).json({ message: 'Logged out successfully' });
     } catch (error) {
       logger.error('Error during logout:', { error });
